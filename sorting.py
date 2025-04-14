4915cdf09dfc4e668df9e6ae8bec7036d6faf531
Borbala Udvaros <udvarosb1@gmail.com>
2025-04-14 13:06:11 +0200

    update2

--- sorting.py
+++ sorting.py
@@ -22,9 +22,40 @@ def read_data(file_name):
     return data
 
 
+
+def selection_sort(number_array, direction='ascending'):
+    """
+    :param list number_array: list with numeric array
+    :param string direction: string indicating sorting direction: ascending / descending
+    :return: sorted numeric array
+    """
+
+    n = len(number_array)
+    for i in range(n):
+        min_max_idx = i
+        for num_idx in range(i+1,n):
+             if direction == "ascending":
+                 if number_array[num_idx] < number_array [min_max_idx]:
+                    min_max_idx = num_idx
+
+             elif direction == "descending":
+                 if number_array[num_idx] > number_array[min_max_idx]:
+                     min_max_idx = num_idx
+
+        number_array[i], number_array[min_max_idx] = number_array[min_max_idx], number_array[i]
+
+    return number_array
+
+
+
+
+
 def main():
     data = read_data("numbers.csv")
     print(data)
+    sorted_data = selection_sort(data["series_1"])
+    print(sorted_data)
+
 
 
 
