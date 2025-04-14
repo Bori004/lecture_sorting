6d17984d54eed25d255dfc90db986aa449a7e8f5
Borbala Udvaros <udvarosb1@gmail.com>
2025-04-14 14:01:40 +0200

    update

--- sorting.py
+++ sorting.py
@@ -47,14 +47,69 @@ def selection_sort(number_array, direction='ascending'):
     return number_array
 
 
+def bubble_sort(number_array):
+    """
+    :param str number_array: list with numeric array
+    :return: sorted numeric array
+    """
+    n = len(number_array)
+    switched = True
+    print(number_array)
+    while switched:
+        switched = False
+        for i in range(n-1):
+            number = number_array[i]
+            if number_array[i] > number_array[i+1]:
+                number_array[i] = number_array[i+1]
+                number_array[i+1] = number
+                switched = True
+            print(number_array)
+        print(10*"*")
+        i +=1
+
+
+    return number_array
+
+
+
+# def insertion_sort(number_array):
+#     n = len(number_array)
+#     for i in range(n-1):
+#         print(5*"*")
+#         print(number_array[i])
+#         print(5*"*")
+#         for number in number_array[(i+1):(n-1)]:
+#             print(number)
+#             if number < number_array[i]:
+#                 print(True)
+#                 for j in number_array[:i+1]:
+#                     print(5*"/")
+#                     print(j)
+#                     if number_array[j]> number:
+#                         number_array.insert(number, j)
+
+def insertion_sort(number_array):
+    n = len(number_array)
+    for i in range(1,n):
+        key = number_array[i]
+        j = i-1
+        while j>= 0 and number_array[j] > key:
+            number_array[j+1] = number_array[j]
+            j = j-1
+        number_array[j+1] = key
+    return number_array
 
 
 
 def main():
     data = read_data("numbers.csv")
     print(data)
-    sorted_data = selection_sort(data["series_1"])
-    print(sorted_data)
+    # sorted_data = selection_sort(data["series_1"])
+    # print(sorted_data)
+    # bubbled_data = bubble_sort(data["series_1"])
+    # print(bubbled_data)
+    inserted_data = insertion_sort(data["series_1"])
+    print(inserted_data)
 
 
 
